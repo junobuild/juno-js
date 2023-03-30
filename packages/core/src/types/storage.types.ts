ad90d65946a16bb058fa377136b7ad3b2ced0ab7
David Dal Busco <david.dalbusco@outlook.com>
2023-03-30 15:01:59 +0200

    feat: add timestamps to Asset type (#16)

--- packages/core/src/types/storage.types.ts
+++ packages/core/src/types/storage.types.ts
@@ -19,6 +19,8 @@ export interface Asset extends AssetKey {
   token?: string;
   headers: [string, string][];
   encodings: Record<ENCODING_TYPE, AssetEncoding>;
+  created_at?: bigint;
+  updated_at?: bigint;
 }
 
 export interface Assets extends Pick<ListResults<AssetNoContent>, 'length' | 'matches_length'> {

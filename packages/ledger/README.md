bd9dc2a0c03676b54c34fb49879b2d06eba26144
David Dal Busco <david.dalbusco@outlook.com>
2023-05-01 15:32:54 +0200

    feat: icp ledger (#24)

---
+++ packages/ledger/README.md
@@ -0,0 +1,17 @@
+[![npm][npm-badge]][npm-badge-url]
+[![license][npm-license]][npm-license-url]
+
+[npm-badge]: https://img.shields.io/npm/v/@junobuild/ledger
+[npm-badge-url]: https://www.npmjs.com/package/@junobuild/ledger
+[npm-license]: https://img.shields.io/npm/l/@junobuild/ledger
+[npm-license-url]: https://github.com/deckgo/junobuild/blob/main/webcomponents/core/LICENSE
+
+# Juno JavaScript core SDK
+
+A fork of [@dfinity/nns](https://github.com/dfinity/ic-js/tree/main/packages/nns) that includes only the necessary parts, specifically the `AccountIdentifier`, used in the Juno console to minimize the bundle size.
+
+## License
+
+MIT © [David Dal Busco](mailto:david.dalbusco@outlook.com)
+
+[juno]: https://juno.build
--- scripts/esbuild.mjs
+++ scripts/esbuild.mjs
@@ -2,7 +2,7 @@
 
 import {NodeModulesPolyfillPlugin} from '@esbuild-plugins/node-modules-polyfill';
 import esbuild from 'esbuild';
-import {existsSync, mkdirSync, readdirSync, statSync, writeFileSync} from 'fs';
+import {existsSync, mkdirSync, readdirSync, readFileSync, statSync, writeFileSync} from 'fs';
 import {join} from 'path';
 
 const dist = join(process.cwd(), 'dist');
@@ -12,6 +12,15 @@ const createDistFolder = () => {
     mkdirSync(dist);
   }
 };
+
+// Skip peer dependencies
+const peerDependencies = (packageJson) => {
+  const json = readFileSync(packageJson, 'utf8');
+  const {peerDependencies} = JSON.parse(json);
+  return peerDependencies ?? {};
+};
+
+const workspacePeerDependencies = peerDependencies(join(process.cwd(), 'package.json'));
 
 const buildEsmCjs = () => {
   const entryPoints = readdirSync(join(process.cwd(), 'src'))
@@ -36,7 +45,8 @@ const buildEsmCjs = () => {
       format: 'esm',
       define: {global: 'window'},
       target: ['esnext'],
-      plugins: [NodeModulesPolyfillPlugin()]
+      plugins: [NodeModulesPolyfillPlugin()],
+      external: [...Object.keys(workspacePeerDependencies)]
     })
     .catch(() => process.exit(1));
 
@@ -53,7 +63,8 @@ const buildEsmCjs = () => {
       target: ['node18', 'esnext'],
       banner: {
         js: "import { createRequire as topLevelCreateRequire } from 'module';\n const require = topLevelCreateRequire(import.meta.url);"
-      }
+      },
+      external: [...Object.keys(workspacePeerDependencies)]
     })
     .catch(() => process.exit(1));
 };

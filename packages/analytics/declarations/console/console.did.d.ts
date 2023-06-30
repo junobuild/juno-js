84507fd0a34988d2578fd49512386165a5d4ccef
David Dal Busco <david.dalbusco@outlook.com>
2023-06-30 11:49:43 +0200

    build: copy declarations

---
+++ packages/analytics/declarations/console/console.did.d.ts
@@ -0,0 +1,67 @@
+import type {ActorMethod} from '@dfinity/agent';
+import type {Principal} from '@dfinity/principal';
+
+export interface AssertMissionControlCenterArgs {
+  mission_control_id: Principal;
+  user: Principal;
+}
+export type ControllerScope = {Write: null} | {Admin: null};
+export interface CreateSatelliteArgs {
+  block_index: [] | [bigint];
+  user: Principal;
+}
+export interface DeleteControllersArgs {
+  controllers: Array<Principal>;
+}
+export interface GetCreateSatelliteFeeArgs {
+  user: Principal;
+}
+export interface LoadRelease {
+  total: bigint;
+  chunks: bigint;
+}
+export interface MissionControl {
+  updated_at: bigint;
+  credits: Tokens;
+  mission_control_id: [] | [Principal];
+  owner: Principal;
+  created_at: bigint;
+}
+export interface RateConfig {
+  max_tokens: bigint;
+  time_per_token_ns: bigint;
+}
+export interface ReleasesVersion {
+  satellite: [] | [string];
+  mission_control: [] | [string];
+}
+export type Segment = {MissionControl: null} | {Satellite: null};
+export interface SetController {
+  metadata: Array<[string, string]>;
+  scope: ControllerScope;
+  expires_at: [] | [bigint];
+}
+export interface SetControllersArgs {
+  controller: SetController;
+  controllers: Array<Principal>;
+}
+export interface Tokens {
+  e8s: bigint;
+}
+export interface _SERVICE {
+  add_invitation_code: ActorMethod<[string], undefined>;
+  assert_mission_control_center: ActorMethod<[AssertMissionControlCenterArgs], undefined>;
+  create_satellite: ActorMethod<[CreateSatelliteArgs], Principal>;
+  del_controllers: ActorMethod<[DeleteControllersArgs], undefined>;
+  get_create_satellite_fee: ActorMethod<[GetCreateSatelliteFeeArgs], [] | [Tokens]>;
+  get_credits: ActorMethod<[], Tokens>;
+  get_releases_version: ActorMethod<[], ReleasesVersion>;
+  get_user_mission_control_center: ActorMethod<[], [] | [MissionControl]>;
+  init_user_mission_control_center: ActorMethod<[], MissionControl>;
+  list_user_mission_control_centers: ActorMethod<[], Array<[Principal, MissionControl]>>;
+  load_release: ActorMethod<[Segment, Uint8Array | number[], string], LoadRelease>;
+  reset_release: ActorMethod<[Segment], undefined>;
+  set_controllers: ActorMethod<[SetControllersArgs], undefined>;
+  update_rate_config: ActorMethod<[Segment, RateConfig], undefined>;
+  version: ActorMethod<[], string>;
+}
---
+++ packages/analytics/declarations/observatory/observatory.did.d.ts
@@ -0,0 +1,84 @@
+import type {ActorMethod} from '@dfinity/agent';
+import type {Principal} from '@dfinity/principal';
+
+export interface CanisterStatusResponse {
+  status: CanisterStatusType;
+  memory_size: bigint;
+  cycles: bigint;
+  settings: DefiniteCanisterSettings;
+  idle_cycles_burned_per_day: bigint;
+  module_hash: [] | [Uint8Array | number[]];
+}
+export type CanisterStatusType = {stopped: null} | {stopping: null} | {running: null};
+export type ControllerScope = {Write: null} | {Admin: null};
+export interface CronJobStatuses {
+  mission_control_cycles_threshold: [] | [bigint];
+  satellites: Array<[Principal, CronJobStatusesSatelliteConfig]>;
+  enabled: boolean;
+  cycles_threshold: [] | [bigint];
+}
+export interface CronJobStatusesSatelliteConfig {
+  enabled: boolean;
+  cycles_threshold: [] | [bigint];
+}
+export interface CronJobs {
+  metadata: Array<[string, string]>;
+  statuses: CronJobStatuses;
+}
+export interface CronTab {
+  cron_jobs: CronJobs;
+  updated_at: bigint;
+  mission_control_id: Principal;
+  created_at: bigint;
+}
+export interface DefiniteCanisterSettings {
+  freezing_threshold: bigint;
+  controllers: Array<Principal>;
+  memory_allocation: bigint;
+  compute_allocation: bigint;
+}
+export interface DeleteControllersArgs {
+  controllers: Array<Principal>;
+}
+export interface ListStatuses {
+  cron_jobs: CronJobs;
+  statuses: Result_1;
+  timestamp: bigint;
+}
+export interface ListStatusesArgs {
+  time_delta: [] | [bigint];
+}
+export type Result = {Ok: SegmentStatus} | {Err: string};
+export type Result_1 = {Ok: SegmentsStatuses} | {Err: string};
+export interface SegmentStatus {
+  id: Principal;
+  status: CanisterStatusResponse;
+  metadata: [] | [Array<[string, string]>];
+  status_at: bigint;
+}
+export interface SegmentsStatuses {
+  satellites: [] | [Array<Result>];
+  mission_control: Result;
+}
+export interface SetController {
+  metadata: Array<[string, string]>;
+  scope: ControllerScope;
+  expires_at: [] | [bigint];
+}
+export interface SetControllersArgs {
+  controller: SetController;
+  controllers: Array<Principal>;
+}
+export interface SetCronTab {
+  cron_jobs: CronJobs;
+  updated_at: [] | [bigint];
+  mission_control_id: Principal;
+}
+export interface _SERVICE {
+  del_controllers: ActorMethod<[DeleteControllersArgs], undefined>;
+  get_cron_tab: ActorMethod<[], [] | [CronTab]>;
+  list_statuses: ActorMethod<[ListStatusesArgs], Array<ListStatuses>>;
+  set_controllers: ActorMethod<[SetControllersArgs], undefined>;
+  set_cron_tab: ActorMethod<[SetCronTab], CronTab>;
+  version: ActorMethod<[], string>;
+}

3a72ee16264182c0678a0938880268adda8aaa7e
David Dal Busco <david.dalbusco@outlook.com>
2023-11-07 17:42:03 +0100

    build: agent-js as peer dependency of core (#77)

--- packages/core/esbuild.mjs
+++ packages/core/esbuild.mjs
@@ -1,11 +1,20 @@
 #!/usr/bin/env node
 
 import esbuild from 'esbuild';
-import {readdirSync, statSync} from 'fs';
+import {readFileSync, readdirSync, statSync} from 'fs';
 import {join} from 'path';
 import {build} from '../../scripts/esbuild.mjs';
 
 build();
+
+// Skip peer dependencies
+const peerDependencies = (packageJson) => {
+  const json = readFileSync(packageJson, 'utf8');
+  const {peerDependencies} = JSON.parse(json);
+  return peerDependencies ?? {};
+};
+
+const workspacePeerDependencies = peerDependencies(join(process.cwd(), 'package.json'));
 
 // Build web workers
 
@@ -27,7 +36,8 @@ const buildWebWorkers = () => {
       bundle: true,
       sourcemap: 'external',
       minify: true,
-      target: ['node18']
+      target: ['node18'],
+      external: [...Object.keys(workspacePeerDependencies)]
     })
     .catch(() => process.exit(1));
 };

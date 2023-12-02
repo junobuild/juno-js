7e2be232a2a491e81fd1ca435562e2aa2e0cc54d
David Dal Busco <david.dalbusco@outlook.com>
2023-12-02 15:19:09 +0100

    feat: support bigint (#92)
    * feat: support bigint
    
    * build: dfinity principal peer dependencies
    
    * build: ignore peer
    
    * build: size

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,4 +1,5 @@
 export * from './utils/debounce.utils';
 export * from './utils/did.utils';
 export * from './utils/env.utils';
+export * from './utils/json.utils';
 export * from './utils/null.utils';
--- packages/utils/src/utils/did.utils.ts
+++ packages/utils/src/utils/did.utils.ts
@@ -1,3 +1,4 @@
+import {jsonReplacer, jsonReviver} from './json.utils';
 import {nonNullish} from './null.utils';
 
 export const toNullable = <T>(value?: T): [] | [T] => {
@@ -9,7 +10,7 @@ export const fromNullable = <T>(value: [] | [T]): T | undefined => {
 };
 
 export const toArray = async <T>(data: T): Promise<Uint8Array> => {
-  const blob: Blob = new Blob([JSON.stringify(data)], {
+  const blob: Blob = new Blob([JSON.stringify(data, jsonReplacer)], {
     type: 'application/json; charset=utf-8'
   });
   return new Uint8Array(await blob.arrayBuffer());
@@ -19,5 +20,5 @@ export const fromArray = async <T>(data: Uint8Array | number[]): Promise<T> => {
   const blob: Blob = new Blob([data instanceof Uint8Array ? data : new Uint8Array(data)], {
     type: 'application/json; charset=utf-8'
   });
-  return JSON.parse(await blob.text());
+  return JSON.parse(await blob.text(), jsonReviver);
 };

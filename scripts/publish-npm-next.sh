0fc088760181d899969bee3349c8122d1b4b2502
David Dal Busco <david.dalbusco@outlook.com>
2023-12-08 17:03:03 +0100

    feat: publish next

---
+++ scripts/publish-npm-next.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+function publish_npm() {
+  local lib=$1
+
+  npm publish --workspace=packages/"$lib" --provenance --access public --tag next
+}
+
+# Tips: libs use by other libs first
+LIBS=utils,core,admin,ledger,analytics,core-peer
+
+for lib in $(echo $LIBS | sed "s/,/ /g"); do
+  publish_npm "$lib"
+done
---
+++ scripts/update-version.mjs
@@ -0,0 +1,70 @@
+import {existsSync, readFileSync, writeFileSync} from 'fs';
+import fetch from 'node-fetch';
+import {join} from 'path';
+
+// The suffix we use to publish to npm wip version of the libs
+const SUFFIX = 'next';
+
+const nextVersion = async ({project, currentVersion}) => {
+  const version = `${currentVersion}-${SUFFIX}-${new Date().toISOString().slice(0, 10)}`;
+
+  const {versions} = await (await fetch(`http://registry.npmjs.org/@junobuild/${project}`)).json();
+
+  // The wip version has never been published
+  if (versions[version] === undefined) {
+    return version;
+  }
+
+  // There was some wip versions already published so, we increment the version number
+  const count = Object.keys(versions).filter((v) => v.includes(version)).length;
+  return `${version}.${count}`;
+};
+
+const updateVersion = async () => {
+  if (process.argv.length !== 3) {
+    console.log('Invalid arguments: node update-version.mjs core|analytics|etc.');
+    return;
+  }
+
+  const project = process.argv[2];
+
+  const packagePath = join(process.cwd(), 'packages', project, 'package.json');
+
+  if (!existsSync(packagePath)) {
+    console.log(`Target ${packagePath} does not exist.`);
+    return;
+  }
+
+  const packageJson = JSON.parse(readFileSync(packagePath, 'utf-8'));
+
+  // Build wip version number
+  const version = await nextVersion({
+    project,
+    currentVersion: packageJson.version
+  });
+
+  // Peer dependencies need to point to wip references - e.g. @junobuild/utils@0.0.1-next
+  const peerDependencies = Object.entries(packageJson.peerDependencies ?? {}).reduce(
+    (acc, [key, _value]) => {
+      acc[key] = `*`;
+      return acc;
+    },
+    {}
+  );
+
+  writeFileSync(
+    packagePath,
+    JSON.stringify(
+      {
+        ...packageJson,
+        version,
+        ...(Object.keys(peerDependencies).length > 0 && {peerDependencies})
+      },
+      null,
+      2
+    ),
+    'utf-8'
+  );
+};
+
+await updateVersion();

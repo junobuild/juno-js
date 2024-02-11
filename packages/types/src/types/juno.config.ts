5ad14c38957de67e847071d603c3e2bc7b251bd4
David Dal Busco <david.dalbusco@outlook.com>
2024-02-11 14:43:52 +0100

    docs: review and add comments

--- packages/types/src/types/juno.config.ts
+++ packages/types/src/types/juno.config.ts
@@ -3,60 +3,65 @@ import {StorageConfig} from './storage.config';
 
 export interface SatelliteAssertions {
   /**
-   * Configuration for the heap memory size check. This can be a boolean to enable or disable the check, or a number to set a custom memory threshold in MB.
+   * Configuration for the heap memory size check, which can be:
+   * - `true` to enable the check with a default threshold of 900MB,
+   * - `false` to disable the heap memory size check,
+   * - A `number` to specify a custom threshold in MB (megabytes) for the heap memory size check.
    *
-   * - true: Enable the default check (with a threshold of 900MB).
-   * - false: Disable the heap memory size check.
-   * - number: Specify a custom threshold (in bytes) for the heap memory size check.
+   * If not specified, then `true` is used as default value.
    */
   heapMemory?: number | boolean;
 }
 
 export interface SatelliteConfig {
   /**
-   * The ID of the satellite to control from the folder where the juno.json config file is located.
-   * e.g. the satellite to which the app should be deployed
+   * The unique identifier (ID) of the satellite where the application will be deployed.
    */
   satelliteId: string;
+
   /**
-   * The "source" attribute specifies which directory to deploy to the storage. Default: build
+   * Specifies the directory from which to deploy to storage. Defaults to 'build'.
+   * For instance, if `npm run build` outputs files to a `dist` folder, use `source: 'dist'`.
    */
   source?: string;
+
   /**
-   * Optional configuration parameters that can be applied to the satellite - i.e. parameters that change the behavior of the satellite on chain.
-   * Any changes to these parameters need to be applied manually afterwards with `npm run ... config`
+   * Optional configuration parameters for the satellite, affecting the operational behavior of its Storage.
+   * Changes to these parameters must be applied manually afterwards using `juno config` commands.
    */
   storage?: StorageConfig;
+
   /**
-   * The "ignore" attribute specifies the files to ignore on deploy. It can take globs the same way that Git handles .gitignore.
+   * Specifies files or patterns to ignore during deployment, using glob patterns similar to those in .gitignore.
    */
   ignore?: string[];
+
   /**
-   * By default, the CLI searches for JavaScript (js), ES Module (mjs), and CSS (css) files in the source folder and optimizes them with Gzip compression.
-   * You can customize this behavior by either turning it off or providing a different file matching pattern using glob syntax.
+   * Controls the Gzip compression optimization for files in the source folder. By default, it targets JavaScript (js), ES Module (mjs), and CSS (css) files.
+   * You can disable this by setting it to `false` or customize it with a different file matching pattern using glob syntax.
    */
   gzip?: string | false;
+
   /**
-   * The CLI maps the encoding type according to the file extension. Encoding is then used in the satellite to provide the HTTP response header `Content-Encoding`.
-   *
-   * .Z = compress
-   * .gz = gzip
-   * .br = br
-   * .zlib = deflate
-   * rest = identity
-   *
-   * The "encoding" attribute can be used to overwrite the default mapping. It can take globs the same way that Git handles .gitignore.
+   * Customizes file encoding mapping for HTTP response headers `Content-Encoding` based on file extension:
+   * - `.Z` for compress,
+   * - `.gz` for gzip,
+   * - `.br` for brotli,
+   * - `.zlib` for deflate,
+   * - anything else defaults to `identity`.
+   * The "encoding" attribute allows overriding default mappings with an array of glob patterns and encoding types.
    */
   encoding?: Array<[string, ENCODING_TYPE]>;
+
   /**
-   * Optional configuration to override default assertions performed by the CLI.
+   * Optional configurations to override default assertions made by the CLI regarding satellite deployment conditions.
    */
   assertions?: SatelliteAssertions;
 }
 
 export interface OrbiterConfig {
   /**
-   * The ID of the orbiter used in the dapp.
+   * The identifier of the orbiter used in the dApp.
    */
   orbiterId: string;
 }

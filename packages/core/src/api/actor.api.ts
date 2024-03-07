2399bccd0adee380b68fdb78d6ee483ab9d01c20
David Dal Busco <david.dalbusco@outlook.com>
2024-03-07 15:27:09 +0100

    fix: "satelliteUrl" in NodeJS context

--- packages/core/src/api/actor.api.ts
+++ packages/core/src/api/actor.api.ts
@@ -1,29 +1,23 @@
-import {nonNullish} from '@junobuild/utils';
+import {assertNonNullish} from '@junobuild/utils';
 import type {_SERVICE as SatelliteActor} from '../../declarations/satellite/satellite.did';
 import {idlFactory} from '../../declarations/satellite/satellite.factory.did.js';
-import {EnvStore} from '../stores/env.store';
 import type {Satellite} from '../types/satellite.types';
 import {createActor} from '../utils/actor.utils';
+import {mapContainer, mapSatelliteId} from '../utils/env.utils';
 
 export const getSatelliteActor = async ({
-  satelliteId,
+  satelliteId: customSatelliteId,
   container: customContainer,
   ...rest
 }: Satellite): Promise<SatelliteActor> => {
-  const {satelliteId: canisterId} = nonNullish(satelliteId)
-    ? {satelliteId}
-    : EnvStore.getInstance().get() ?? {satelliteId: undefined};
+  const {satelliteId} = mapSatelliteId({satelliteId: customSatelliteId});
 
-  const {container} = nonNullish(customContainer)
-    ? {container: customContainer}
-    : EnvStore.getInstance().get() ?? {container: undefined};
+  assertNonNullish(satelliteId, 'No satellite principal defined.');
 
-  if (!canisterId) {
-    throw new Error('No satellite principal defined.');
-  }
+  const {container} = mapContainer({container: customContainer});
 
   return createActor({
-    satelliteId: canisterId,
+    satelliteId,
     container,
     idlFactory,
     ...rest

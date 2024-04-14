057f1cb3d579c977aceb2aae8150e9dac0b6a7c9
David Dal Busco <david.dalbusco@outlook.com>
2024-04-14 09:15:51 +0200

    feat: move cli tools used in cli and create-juno (#137)

--- packages/cli-tools/src/index.ts
+++ packages/cli-tools/src/index.ts
@@ -1,2 +1,7 @@
 export * from './configs/juno.config';
 export type * from './types/config';
+export * from './utils/args.utils';
+export * from './utils/cmd.utils';
+export * from './utils/compress.utils';
+export * from './utils/download.utils';
+export * from './utils/fs.utils';
---
+++ packages/cli-tools/src/utils/fs.utils.ts
@@ -0,0 +1,22 @@
+import {lstatSync, readdirSync} from 'node:fs';
+import {access} from 'node:fs/promises';
+import {join} from 'node:path';
+
+export const files = (source: string): string[] =>
+  readdirSync(source).flatMap((file) => {
+    const path = join(source, file);
+    return lstatSync(path).isDirectory() ? files(path) : join(path);
+  });
+
+export const fileExists = async (path: string): Promise<boolean> => {
+  try {
+    await access(path);
+    return true;
+  } catch (err: unknown) {
+    if (err instanceof Error && 'code' in err && (err as NodeJS.ErrnoException).code === 'ENOENT') {
+      return false;
+    } else {
+      throw err;
+    }
+  }
+};

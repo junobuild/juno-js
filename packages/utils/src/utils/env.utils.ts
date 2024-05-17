132f398ed042ce157d15a1c676ba955cb97b892b
David Dal Busco <david.dalbusco@outlook.com>
2024-05-17 12:05:27 +0200

    feat: prefix for env

--- packages/utils/src/utils/env.utils.ts
+++ packages/utils/src/utils/env.utils.ts
@@ -2,5 +2,7 @@ export const isBrowser = () => typeof window !== `undefined`;
 
 export const processEnv = (key: string): string | undefined =>
   typeof process !== 'undefined'
-    ? process.env[`NEXT_PUBLIC_${key}`] ?? process.env[key]
+    ? process.env[`NEXT_PUBLIC_${key}`] ??
+      process.env[`VITE_${key}`] ??
+      process.env[`PUBLIC_${key}`]
     : undefined;

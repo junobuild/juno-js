4396f94faa84ef3a7e4d68613bc84816bc59b7ce
David Dal Busco <david.dalbusco@outlook.com>
2024-05-17 16:45:05 +0200

    feat: use meta import for vite

--- packages/utils/src/utils/env.utils.ts
+++ packages/utils/src/utils/env.utils.ts
@@ -1,22 +1,30 @@
 export const isBrowser = () => typeof window !== `undefined`;
 
+type ImportMeta = {env: Record<string, string>};
+
 export const processEnvSatelliteId = (): string | undefined =>
   typeof process !== 'undefined'
-    ? process.env.NEXT_PUBLIC_SATELLITE_ID ??
-      process.env.VITE_SATELLITE_ID ??
-      process.env.PUBLIC_SATELLITE_ID
-    : undefined;
+    ? process.env?.NEXT_PUBLIC_SATELLITE_ID
+    : typeof import.meta !== undefined &&
+        typeof (import.meta as unknown as ImportMeta).env !== undefined
+      ? (import.meta as unknown as ImportMeta).env.VITE_SATELLITE_ID ??
+        (import.meta as unknown as ImportMeta).env.PUBLIC_SATELLITE_ID
+      : undefined;
 
 export const processEnvOrbiterId = (): string | undefined =>
   typeof process !== 'undefined'
-    ? process.env.NEXT_PUBLIC_ORBITER_ID ??
-      process.env.VITE_ORBITER_ID ??
-      process.env.PUBLIC_ORBITER_ID
-    : undefined;
+    ? process.env?.NEXT_PUBLIC_ORBITER_ID
+    : typeof import.meta !== undefined &&
+        typeof (import.meta as unknown as ImportMeta).env !== undefined
+      ? (import.meta as unknown as ImportMeta).env.VITE_ORBITER_ID ??
+        (import.meta as unknown as ImportMeta).env.ORBITER_ID
+      : undefined;
 
 export const processEnvContainer = (): string | undefined =>
   typeof process !== 'undefined'
-    ? process.env.NEXT_PUBLIC_CONTAINER ??
-      process.env.VITE_CONTAINER ??
-      process.env.PUBLIC_CONTAINER
-    : undefined;
+    ? process.env?.NEXT_PUBLIC_CONTAINER
+    : typeof import.meta !== undefined &&
+        typeof (import.meta as unknown as ImportMeta).env !== undefined
+      ? (import.meta as unknown as ImportMeta).env.CONTAINER ??
+        (import.meta as unknown as ImportMeta).env.CONTAINER
+      : undefined;

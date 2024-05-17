e3421e26a254f7b3554ab38331f55998035b180b
David Dal Busco <david.dalbusco@outlook.com>
2024-05-17 17:50:07 +0200

    feat: safer

--- packages/utils/src/utils/env.utils.ts
+++ packages/utils/src/utils/env.utils.ts
@@ -1,14 +1,14 @@
 export const isBrowser = () => typeof window !== `undefined`;
 
-type ImportMeta = {env: Record<string, string>};
+type ImportMeta = {env: Record<string, string> | undefined};
 
 export const processEnvSatelliteId = (): string | undefined =>
   typeof process !== 'undefined'
     ? process.env?.NEXT_PUBLIC_SATELLITE_ID
     : typeof import.meta !== 'undefined' &&
         typeof (import.meta as unknown as ImportMeta).env !== 'undefined'
-      ? (import.meta as unknown as ImportMeta).env.VITE_SATELLITE_ID ??
-        (import.meta as unknown as ImportMeta).env.PUBLIC_SATELLITE_ID
+      ? (import.meta as unknown as ImportMeta).env?.VITE_SATELLITE_ID ??
+        (import.meta as unknown as ImportMeta).env?.PUBLIC_SATELLITE_ID
       : undefined;
 
 export const processEnvOrbiterId = (): string | undefined =>
@@ -16,8 +16,8 @@ export const processEnvOrbiterId = (): string | undefined =>
     ? process.env?.NEXT_PUBLIC_ORBITER_ID
     : typeof import.meta !== 'undefined' &&
         typeof (import.meta as unknown as ImportMeta).env !== 'undefined'
-      ? (import.meta as unknown as ImportMeta).env.VITE_ORBITER_ID ??
-        (import.meta as unknown as ImportMeta).env.PUBLIC_ORBITER_ID
+      ? (import.meta as unknown as ImportMeta).env?.VITE_ORBITER_ID ??
+        (import.meta as unknown as ImportMeta).env?.PUBLIC_ORBITER_ID
       : undefined;
 
 export const processEnvContainer = (): string | undefined =>
@@ -25,6 +25,6 @@ export const processEnvContainer = (): string | undefined =>
     ? process.env?.NEXT_PUBLIC_CONTAINER
     : typeof import.meta !== 'undefined' &&
         typeof (import.meta as unknown as ImportMeta).env !== 'undefined'
-      ? (import.meta as unknown as ImportMeta).env.VITE_CONTAINER ??
-        (import.meta as unknown as ImportMeta).env.PUBLIC_CONTAINER
+      ? (import.meta as unknown as ImportMeta).env?.VITE_CONTAINER ??
+        (import.meta as unknown as ImportMeta).env?.PUBLIC_CONTAINER
       : undefined;

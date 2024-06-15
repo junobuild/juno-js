58ce0fbe47d74893dff1003973a992c059da25fc
David Dal Busco <david.dalbusco@outlook.com>
2024-06-15 10:58:07 +0200

    feat: cli config for console

--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -1,10 +1,12 @@
+export type * from './console/console.config';
 export type * from './dev/juno.dev.config';
 export type * from './mainnet/authentication.config';
 export type * from './mainnet/juno.config';
-export type * from './mainnet/juno.env';
 export type * from './mainnet/satellite.config';
 export type * from './mainnet/storage.config';
+export type * from './types/cli.config';
 export type * from './types/encoding';
+export type * from './types/juno.env';
 export type * from './types/rules';
 export type * from './utils/ts.utils';
 

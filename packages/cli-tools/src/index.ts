64bd4f471c5e0e8c6710584bc8883f8c934af80b
David Dal Busco <david.dalbusco@outlook.com>
2024-06-20 07:23:51 +0200

    feat: move config reader from cli-tools to dedicated lib

--- packages/cli-tools/src/index.ts
+++ packages/cli-tools/src/index.ts
@@ -1,5 +1,3 @@
-export * from './configs/juno.config';
-export type * from './types/config';
 export * from './utils/args.utils';
 export * from './utils/cmd.utils';
 export * from './utils/compress.utils';

e24a46d2e1930213effcb7c51e1776ad40ca5027
David Dal Busco <david.dalbusco@outlook.com>
2024-06-22 14:59:50 +0200

    feat: expose actor types

--- packages/console/src/index.ts
+++ packages/console/src/index.ts
@@ -1,4 +1,5 @@
 export type * from '../declarations/console/console.did';
 export * from './api/proposal.api';
 export * from './api/storage.api';
+export type * from './types/actor.types';
 export type * from './types/console.types';

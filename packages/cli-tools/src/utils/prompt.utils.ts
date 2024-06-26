9d39e91da0523c8b98f8ffbd4d41cb9a0392b3eb
David Dal Busco <david.dalbusco@outlook.com>
2024-06-26 20:27:06 +0200

    feat: remove kleur

--- packages/cli-tools/src/utils/prompt.utils.ts
+++ packages/cli-tools/src/utils/prompt.utils.ts
@@ -1,5 +1,4 @@
 import {isNullish, nonNullish} from '@junobuild/utils';
-import {red} from 'kleur';
 
 // In case an answer is replaced by control+c
 export const assertAnswerCtrlC: (
@@ -11,7 +10,7 @@ export const assertAnswerCtrlC: (
 ): void => {
   if (isNullish(answer) || answer === '') {
     if (nonNullish(message)) {
-      console.log(`${red(message)}`);
+      console.error(message);
     }
 
     process.exit(1);

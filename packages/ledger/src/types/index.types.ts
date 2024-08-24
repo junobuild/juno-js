3bf45eeb5e054b2ab01c0a5fd5af1d7fb0c4ecca
David Dal Busco <david.dalbusco@outlook.com>
2024-08-24 10:53:07 +0200

    feat: deprecate junobuild/ledger

--- packages/ledger/src/types/index.types.ts
+++
@@ -1,5 +0,0 @@
-import type {GetAccountIdentifierTransactionsArgs} from '../../declarations/index/index.did';
-
-export type TransactionsArgs = Omit<GetAccountIdentifierTransactionsArgs, 'start'> & {
-  start?: bigint;
-};

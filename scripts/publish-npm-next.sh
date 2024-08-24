0e01f7f73116128f10b6b47175de3f621a2e79df
David Dal Busco <david.dalbusco@outlook.com>
2024-08-24 10:56:16 +0200

    chore: cleanup ledger

--- scripts/publish-npm-next.sh
+++ scripts/publish-npm-next.sh
@@ -7,7 +7,7 @@ function publish_npm() {
 }
 
 # Tips: libs use by other libs first
-LIBS=utils,config,config-loader,cli-tools,storage,core,admin,console,ledger,analytics,core-peer
+LIBS=utils,config,config-loader,cli-tools,storage,core,admin,console,analytics,core-peer
 
 for lib in $(echo $LIBS | sed "s/,/ /g"); do
   publish_npm "$lib"

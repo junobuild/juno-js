6a68a50a529b836a982ab7b6f27b7cb42824fc45
David Dal Busco <david.dalbusco@outlook.com>
2024-09-11 21:29:56 +0200

    build: setup vitest

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import {defineConfig} from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    environment: 'jsdom',
+    include: ['packages/src/**/*.spec.ts'],
+    globals: true,
+    watch: false,
+    passWithNoTests: true,
+  }
+});

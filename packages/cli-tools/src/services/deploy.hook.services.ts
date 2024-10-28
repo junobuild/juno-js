49765e19ff83277bc7b030e5c166cc23698aecbf
David Dal Busco <david.dalbusco@outlook.com>
2024-10-28 21:38:02 +0100

    fix: it's late

--- packages/cli-tools/src/services/deploy.hook.services.ts
+++ packages/cli-tools/src/services/deploy.hook.services.ts
@@ -1,7 +1,7 @@
 import {execute} from '../utils/cmd.utils';
 
 export const executeHooks = async (hooks?: string[]) => {
-  for (const hook in hooks ?? []) {
+  for (const hook of hooks ?? []) {
     const [cmd, ...args] = hook.split(' ');
     await execute({command: cmd, args});
   }

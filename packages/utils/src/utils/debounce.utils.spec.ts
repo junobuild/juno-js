c0799d51e87326d28542e57cf192d987bc775473
David Dal Busco <david.dalbusco@outlook.com>
2025-01-28 13:20:46 +0100

    feat: update utils as in ic-js

---
+++ packages/utils/src/utils/debounce.utils.spec.ts
@@ -0,0 +1,59 @@
+import type {Mock} from 'vitest';
+import {debounce} from './debounce.utils';
+
+describe('debounce-utils', () => {
+  let callback: Mock;
+
+  beforeAll(() => vi.spyOn(console, 'error').mockImplementation(() => undefined));
+
+  afterAll(() => vi.resetAllMocks());
+
+  beforeEach(() => {
+    vi.useFakeTimers();
+    vi.spyOn(global, 'setTimeout');
+    callback = vi.fn();
+  });
+
+  afterEach(() => vi.useRealTimers());
+
+  it('should debounce function with timeout', () => {
+    const testDebounce = debounce(callback, 100);
+
+    testDebounce();
+    testDebounce();
+    testDebounce();
+
+    expect(setTimeout).toHaveBeenCalledTimes(3);
+    expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 100);
+    expect(callback).not.toBeCalled();
+
+    vi.runAllTimers();
+
+    expect(callback).toHaveBeenCalledTimes(1);
+  });
+
+  it('should debounce one function call', () => {
+    debounce(callback)();
+
+    expect(callback).not.toBeCalled();
+
+    vi.runAllTimers();
+
+    expect(callback).toBeCalled();
+    expect(callback).toHaveBeenCalledTimes(1);
+  });
+
+  it('should debounce multiple functions call', () => {
+    const anotherCallback = vi.fn();
+
+    const test = debounce(anotherCallback);
+    test();
+    test();
+    test();
+
+    vi.runAllTimers();
+
+    expect(anotherCallback).toBeCalled();
+    expect(anotherCallback).toHaveBeenCalledTimes(1);
+  });
+});
--- packages/utils/src/utils/did.utils.ts
+++ packages/utils/src/utils/did.utils.ts
@@ -1,21 +1,55 @@
+import {assertNonNullish} from './asserts.utils';
 import {jsonReplacer, jsonReviver} from './json.utils';
-import {nonNullish} from './null.utils';
+import {nonNullish} from './nullish.utils';
 
 /**
- * Converts a value to a nullable array.
- * @template T
- * @param {T} [value] - The value to convert.
- * @returns {([] | [T])} A nullable array containing the value if non-nullish, or an empty array if nullish.
+ * Converts a value into a Candid-style variant representation of an optional value.
+ *
+ * @template T The type of the value.
+ * @param {T | null | undefined} value - The value to convert into a Candid-style variant.
+ * @returns {[] | [T]} A Candid-style variant representation: an empty array for `null` and `undefined` or an array with the value.
  */
-export const toNullable = <T>(value?: T): [] | [T] => (nonNullish(value) ? [value] : []);
+export const toNullable = <T>(value?: T | null): [] | [T] => {
+  return nonNullish(value) ? [value] : [];
+};
 
 /**
- * Extracts a value from a nullable array.
- * @template T
- * @param {([] | [T])} value - The nullable array.
- * @returns {(T | undefined)} The value if present, or undefined if the array is empty.
+ * Extracts the value from a Candid-style variant representation of an optional value.
+ *
+ * @template T The type of the value.
+ * @param {[] | [T]} value - A Candid-style variant representing an optional value.
+ * @returns {T | undefined} The extracted value, or `undefined` if the array is empty.
  */
-export const fromNullable = <T>(value: [] | [T]): T | undefined => value?.[0];
+export const fromNullable = <T>(value: [] | [T]): T | undefined => {
+  return value?.[0];
+};
+
+/**
+ * Extracts the value from a Candid-style variant representation of an optional value,
+ * ensuring the value is defined. Throws an error if the array is empty or the value is nullish.
+ *
+ * @template T The type of the value.
+ * @param {[] | [T]} value - A Candid-style variant representing an optional value.
+ * @returns {T} The extracted value.
+ * @throws {Error} If the array is empty or the value is nullish.
+ */
+export const fromDefinedNullable = <T>(value: [] | [T]): T => {
+  const result = fromNullable(value);
+
+  assertNonNullish(result);
+
+  return result;
+};
+
+/**
+ * Extracts the value from a nullish Candid-style variant representation.
+ *
+ * @template T The type of the value.
+ * @param {([] | [T]) | undefined} value - A Candid-style variant or `undefined`.
+ * @returns {T | undefined} The extracted value, or `undefined` if the input is nullish or the array is empty.
+ */
+export const fromNullishNullable = <T>(value: ([] | [T]) | undefined): T | undefined =>
+  fromNullable(value ?? []);
 
 /**
  * Converts data to a Uint8Array for transmission or storage.

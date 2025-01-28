46a6e372371ff7b7935a20adb533cf7dc1b96280
David Dal Busco <david.dalbusco@outlook.com>
2025-01-28 13:26:42 +0100

    feat: expose nullish

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -3,3 +3,4 @@ export * from './utils/debounce.utils';
 export * from './utils/did.utils';
 export * from './utils/env.utils';
 export * from './utils/json.utils';
+export * from './utils/nullish.utils';

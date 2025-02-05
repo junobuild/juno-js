5710ad7b421329b9409a6a8ebc161cb86850c0ad
David Dal Busco <david.dalbusco@outlook.com>
2025-02-05 21:37:48 +0100

    feat: throw error if client not initialized on signIn

--- packages/core/src/services/auth.services.ts
+++ packages/core/src/services/auth.services.ts
@@ -1,5 +1,6 @@
 import type {Identity} from '@dfinity/agent';
 import type {AuthClient} from '@dfinity/auth-client';
+import {isNullish} from '@dfinity/utils';
 import {
   ALLOW_PIN_AUTHENTICATION,
   DELEGATION_IDENTITY_EXPIRATION
@@ -9,11 +10,11 @@ import {ActorStore} from '../stores/actor.store';
 import {AgentStore} from '../stores/agent.store';
 import {AuthStore} from '../stores/auth.store';
 import type {Provider, SignInOptions} from '../types/auth.types';
+import {SignInError} from '../types/errors.types';
 import {createAuthClient} from '../utils/auth.utils';
 import {initUser} from './user.services';
 
-// eslint-disable-next-line local-rules/use-option-type-wrapper
-let authClient: AuthClient | undefined | null;
+let authClient: AuthClient | undefined;
 
 export const initAuth = async (provider?: Provider) => {
   authClient = authClient ?? (await createAuthClient());
@@ -30,14 +31,22 @@ export const initAuth = async (provider?: Provider) => {
 
 /**
  * Signs in a user with the specified options.
+ *
  * @param {SignInOptions} [options] - The options for signing in.
  * @returns {Promise<void>} A promise that resolves when the sign-in process is complete and the authenticated user is initialized.
- * @throws Will throw an error if the sign-in process fails.
+ * @throws {SignInError} If the sign-in process fails or no authentication client is available.
  */
 export const signIn = (options?: SignInOptions): Promise<void> =>
   /* eslint-disable no-async-promise-executor */
   new Promise<void>(async (resolve, reject) => {
-    authClient = authClient ?? (await createAuthClient());
+    if (isNullish(authClient)) {
+      reject(
+        new SignInError(
+          'No client is ready to perform a sign-in. Have you initialized the Satellite?'
+        )
+      );
+      return;
+    }
 
     const provider = options?.provider ?? new InternetIdentityProvider({});
 
@@ -46,7 +55,7 @@ export const signIn = (options?: SignInOptions): Promise<void> =>
         await initAuth(provider.id);
         resolve();
       },
-      onError: (error?: string) => reject(error),
+      onError: (error?: string) => reject(new SignInError(error)),
       maxTimeToLive: options?.maxTimeToLive ?? DELEGATION_IDENTITY_EXPIRATION,
       allowPinAuthentication: options?.allowPin ?? ALLOW_PIN_AUTHENTICATION,
       ...(options?.derivationOrigin !== undefined && {derivationOrigin: options.derivationOrigin}),
@@ -64,18 +73,12 @@ export const signOut = async (): Promise<void> => {
   await authClient?.logout();
 
   // Reset local object otherwise next sign in (sign in - sign out - sign in) might not work out - i.e. agent-js might not recreate the delegation or identity if not resetted
-  // Technically we do not need this since we recreate the agent below. We just keep it to make the reset explicit.
-  authClient = null;
+  authClient = undefined;
 
   AuthStore.getInstance().reset();
 
   ActorStore.getInstance().reset();
   AgentStore.getInstance().reset();
-
-  // Recreate an HttpClient immediately because next sign-in, if window is not reloaded, would fail if the agent is created within the process.
-  // For example, Safari blocks the Internet Identity (II) window if the agent is created during the interaction.
-  // Agent-js must be created either globally or at least before performing a sign-in.
-  authClient = await createAuthClient();
 };
 
 export const getIdentity = (): Identity | undefined => authClient?.getIdentity();
---
+++ packages/core/src/types/errors.types.ts
@@ -0,0 +1,1 @@
+export class SignInError extends Error {}

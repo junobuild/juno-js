8392515b4aa80d82ff448862ef6661d2c544f649
David Dal Busco <david.dalbusco@outlook.com>
2025-03-12 12:04:15 +0100

    chore: merge main

--- packages/functions/src/global.d.ts
+++ packages/functions/src/global.d.ts
@@ -4,4 +4,3 @@ declare global {
   function __juno_satellite_datastore_raw_data_to_text(data: RawData): string;
   function __juno_satellite_datastore_raw_data_from_text(data: string): RawData;
 }
-

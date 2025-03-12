bea32d7f1ce1a2b10f70e1dcba055c02fd0eb6af
David Dal Busco <david.dalbusco@outlook.com>
2025-03-12 17:46:56 +0100

    test: configs

---
+++ packages/functions/src/tests/configs/assert.config.spec.ts
@@ -0,0 +1,32 @@
+import {defineAssert} from '../../configs/assert.config';
+
+describe('assert.config', () => {
+  const mockAssertSetDoc = vi.fn();
+
+  const mockConfig = {
+    collections: ['users', 'orders'],
+    assertSetDoc: mockAssertSetDoc
+  };
+
+  it('should return the same configuration object if given an object', () => {
+    const result = defineAssert(mockConfig);
+    expect(result).toBe(mockConfig);
+  });
+
+  it('should return the same function if given a function', () => {
+    const mockFn = vi.fn(() => mockConfig);
+    const result = defineAssert(mockFn);
+    expect(result).toBe(mockFn);
+  });
+
+  it('should execute the function and return a configuration when called', () => {
+    const mockFn = vi.fn(() => mockConfig);
+    const result = defineAssert(mockFn);
+    expect(result({})).toBe(mockConfig);
+  });
+
+  it('should not modify the configuration object', () => {
+    const result = defineAssert(mockConfig);
+    expect(result).toEqual(mockConfig);
+  });
+});
---
+++ packages/functions/src/tests/configs/hook.config.spec.ts
@@ -0,0 +1,43 @@
+import {Principal} from '@dfinity/principal';
+import {defineHook} from '../../configs/hook.config';
+import type {DocUpsert} from '../../hooks/datastore';
+
+describe('hook.config', () => {
+  const mockOnSetDoc = vi.fn(async () => {});
+
+  const mockConfig = {
+    collections: ['products', 'transactions'],
+    onSetDoc: mockOnSetDoc
+  };
+
+  it('should return the same configuration object if given an object', () => {
+    const result = defineHook(mockConfig);
+    expect(result).toBe(mockConfig);
+  });
+
+  it('should return the same function if given a function', () => {
+    const mockFn = vi.fn(() => mockConfig);
+    const result = defineHook(mockFn);
+    expect(result).toBe(mockFn);
+  });
+
+  it('should execute the function and return a configuration when called', () => {
+    const mockFn = vi.fn(() => mockConfig);
+    const result = defineHook(mockFn);
+    expect(result({})).toBe(mockConfig);
+  });
+
+  it('should not modify the configuration object', () => {
+    const result = defineHook(mockConfig);
+    expect(result).toEqual(mockConfig);
+  });
+
+  it('should call onSetDoc function when invoked', async () => {
+    const result = defineHook(mockConfig);
+    await result.onSetDoc({
+      caller: Principal.anonymous().toUint8Array(),
+      data: {collection: 'products', key: '123', data: {} as unknown as DocUpsert}
+    });
+    expect(mockOnSetDoc).toHaveBeenCalled();
+  });
+});

20aa35e582c063d2bebefa8d91105fd3cee8d544
David Dal Busco <david.dalbusco@outlook.com>
2025-03-12 11:30:21 +0100

    feat: encode and decode datastore data

---
+++ packages/functions/src/global.d.ts
@@ -0,0 +1,6 @@
+import type {RawData} from './hooks/core';
+
+declare global {
+  function __juno_satellite_datastore_raw_data_to_text(data: RawData): string;
+  function __juno_satellite_datastore_raw_data_from_text(data: string): RawData;
+}

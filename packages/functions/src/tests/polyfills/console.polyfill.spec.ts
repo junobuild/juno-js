47dfe2233d72842659eec896ba6e05d46c206eaa
David Dal Busco <david.dalbusco@outlook.com>
2025-03-14 08:43:51 +0100

    test: repeat test for all types

--- packages/functions/src/tests/polyfills/console.polyfill.spec.ts
+++ packages/functions/src/tests/polyfills/console.polyfill.spec.ts
@@ -7,33 +7,44 @@ describe('Console', () => {
     vi.clearAllMocks();
   });
 
-  it('should call __ic_cdk_print when console.info is used', () => {
-    console.info('Hello', 'World');
-    expect(globalThis.__ic_cdk_print).toHaveBeenCalledWith('Hello World');
-  });
+  const options = [
+    {
+      title: 'log',
+      fn: console.log
+    },
+    {
+      title: 'info',
+      fn: console.info
+    },
+    {
+      title: 'warn',
+      fn: console.warn
+    },
+    {
+      title: 'error',
+      fn: console.error
+    }
+  ];
 
-  it('should call __ic_cdk_print when console.log is used', () => {
-    console.log('Test message');
-    expect(globalThis.__ic_cdk_print).toHaveBeenCalledWith('Test message');
-  });
+  describe.each(options)('$title', ({fn}) => {
+    it('should call __ic_cdk_print', () => {
+      fn('Hello', 'World');
+      expect(globalThis.__ic_cdk_print).toHaveBeenCalledWith('Hello World');
+    });
 
-  it('should call __ic_cdk_print when console.warn is used', () => {
-    console.warn('Warning message');
-    expect(globalThis.__ic_cdk_print).toHaveBeenCalledWith('Warning message');
-  });
+    it('should call __ic_cdk_print with a string', () => {
+      fn('Test message');
+      expect(globalThis.__ic_cdk_print).toHaveBeenCalledWith('Test message');
+    });
 
-  it('should call __ic_cdk_print when console.error is used', () => {
-    console.error('Error message');
-    expect(globalThis.__ic_cdk_print).toHaveBeenCalledWith('Error message');
-  });
+    it('should stringify objects before logging', () => {
+      fn({key: 'value'});
+      expect(globalThis.__ic_cdk_print).toHaveBeenCalledWith('{"key":"value"}');
+    });
 
-  it('should stringify objects before logging', () => {
-    console.log({key: 'value'});
-    expect(globalThis.__ic_cdk_print).toHaveBeenCalledWith('{"key":"value"}');
-  });
-
-  it('should join multiple arguments correctly', () => {
-    console.log('Number:', 42, {foo: 'bar'});
-    expect(globalThis.__ic_cdk_print).toHaveBeenCalledWith('Number: 42 {"foo":"bar"}');
+    it('should join multiple arguments correctly', () => {
+      fn('Number:', 42, {foo: 'bar'});
+      expect(globalThis.__ic_cdk_print).toHaveBeenCalledWith('Number: 42 {"foo":"bar"}');
+    });
   });
 });

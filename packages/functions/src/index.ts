33876150f3722c1ec49e1590de9b6da1e60df675
David Dal Busco <david.dalbusco@outlook.com>
2025-03-29 13:32:11 +0100

    fix: export types

--- packages/functions/src/index.ts
+++ packages/functions/src/index.ts
@@ -5,8 +5,11 @@ export * from './hooks/schemas/context';
 export * from './hooks/schemas/db/context';
 export * from './hooks/schemas/db/payload';
 export * from './hooks/schemas/satellite.env';
+export * from './hooks/schemas/storage/context';
+export * from './hooks/schemas/storage/payload';
 export * from './schemas/candid';
 export * from './schemas/db';
 export * from './schemas/satellite';
+export * from './schemas/storage';
 
 import './polyfills/console.polyfill';

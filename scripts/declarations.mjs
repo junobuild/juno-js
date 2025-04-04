b0166971a2af1ec74e7d0768103decb9f0bc6ad8
David Dal Busco <david.dalbusco@outlook.com>
2025-04-04 15:26:22 +0200

    chore: remove did deprecated from storage lib (#316)

--- scripts/declarations.mjs
+++ scripts/declarations.mjs
@@ -22,7 +22,7 @@ const corePromises = coreDeclarations.map((d) =>
 );
 await Promise.all(corePromises);
 
-const storageDeclarations = ['satellite', 'console', 'deprecated'];
+const storageDeclarations = ['satellite', 'console'];
 const storagePromises = storageDeclarations.map((d) =>
   cp(join(src, d), join('./packages/storage/declarations/', d), {recursive: true})
 );

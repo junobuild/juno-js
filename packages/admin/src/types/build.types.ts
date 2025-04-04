5c1d36737539c238da8d9ae5af1aa48983e92685
David Dal Busco <david.dalbusco@outlook.com>
2025-04-04 13:52:36 +0200

    feat: support for build type sputnik

--- packages/admin/src/types/build.types.ts
+++ packages/admin/src/types/build.types.ts
@@ -1,5 +1,5 @@
 /**
  * Represents the type of build.
- * @typedef {'stock' | 'extended'} BuildType
+ * @typedef {'stock' | 'extended' | 'sputnik'} BuildType
  */
-export type BuildType = 'stock' | 'extended';
+export type BuildType = 'stock' | 'extended' | 'sputnik';
--- packages/core/src/types/build.types.ts
+++ packages/core/src/types/build.types.ts
@@ -1,6 +1,6 @@
 /**
  * Represents the type of build.
- * @typedef {'stock' | 'extended'} BuildType
+ * @typedef {'stock' | 'extended' | 'sputnik'} BuildType
  */
 // TODO: Duplicate type of admin package.
-export type BuildType = 'stock' | 'extended';
+export type BuildType = 'stock' | 'extended' | 'sputnik';

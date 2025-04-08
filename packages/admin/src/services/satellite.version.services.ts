e4909c4bf7953efc7e852715504ffaa1ad683261
David Dal Busco <david.dalbusco@outlook.com>
2025-04-08 17:56:16 +0200

    feat: use juno:package to fetch Satellite version

--- packages/admin/src/services/satellite.version.services.ts
+++ packages/admin/src/services/satellite.version.services.ts
@@ -1,8 +1,12 @@
-import {nonNullish} from '@dfinity/utils';
+import {assertNonNullish, isNullish, nonNullish} from '@dfinity/utils';
+import {JUNO_PACKAGE_SATELLITE_ID} from '@junobuild/config';
 import {canisterMetadata} from '../api/ic.api';
 import {version} from '../api/satellite.api';
+import {SatelliteMissingDependencyError} from '../errors/version.errors';
 import type {SatelliteParameters} from '../types/actor.types';
 import type {BuildType} from '../types/build.types';
+import {findJunoPackageDependency} from '../utils/package.utils';
+import {getJunoPackage} from './package.services';
 
 /**
  * Retrieves the version of the satellite.
@@ -10,8 +14,41 @@ import type {BuildType} from '../types/build.types';
  * @param {SatelliteParameters} params.satellite - The satellite parameters.
  * @returns {Promise<string>} A promise that resolves to the version of the satellite.
  */
-export const satelliteVersion = (params: {satellite: SatelliteParameters}): Promise<string> =>
-  version(params);
+export const satelliteVersion = async ({
+  satellite: {satelliteId, ...rest}
+}: {
+  satellite: SatelliteParameters;
+}): Promise<string> => {
+  assertNonNullish(satelliteId, 'A Satellite ID must be provided to request its version.');
+
+  const pkg = await getJunoPackage({
+    moduleId: satelliteId,
+    ...rest
+  });
+
+  // For backwards compatibility with legacy. Function version() was deprecated in Satellite > v0.0.22
+  if (isNullish(pkg)) {
+    return await version({satellite: {satelliteId, ...rest}});
+  }
+
+  const {name, version: satelliteVersion, dependencies} = pkg;
+
+  if (name === JUNO_PACKAGE_SATELLITE_ID) {
+    return satelliteVersion;
+  }
+
+  const satelliteDependency = findJunoPackageDependency({
+    dependencies,
+    dependencyId: JUNO_PACKAGE_SATELLITE_ID
+  });
+
+  if (nonNullish(satelliteDependency)) {
+    const [_, satelliteVersion] = satelliteDependency;
+    return satelliteVersion;
+  }
+
+  throw new SatelliteMissingDependencyError();
+};
 
 /**
  * Retrieves the build type of the satellite.
@@ -23,6 +60,44 @@ export const satelliteBuildType = async ({
   satellite: {satelliteId, ...rest}
 }: {
   satellite: SatelliteParameters;
+}): Promise<BuildType | undefined> => {
+  assertNonNullish(satelliteId, 'A Satellite ID must be provided to request its version.');
+
+  const pkg = await getJunoPackage({
+    moduleId: satelliteId,
+    ...rest
+  });
+
+  if (isNullish(pkg)) {
+    return await satelliteDeprecatedBuildType({satellite: {satelliteId, ...rest}});
+  }
+
+  const {name, dependencies} = pkg;
+
+  if (name === JUNO_PACKAGE_SATELLITE_ID) {
+    return 'stock';
+  }
+
+  const satelliteDependency = findJunoPackageDependency({
+    dependencies,
+    dependencyId: JUNO_PACKAGE_SATELLITE_ID
+  });
+
+  if (nonNullish(satelliteDependency)) {
+    return 'extended';
+  }
+
+  throw new SatelliteMissingDependencyError();
+};
+
+/**
+ * @deprecated Replaced in Satellite > v0.0.22
+ */
+const satelliteDeprecatedBuildType = async ({
+  satellite: {satelliteId, ...rest}
+}: {
+  satellite: Omit<SatelliteParameters, 'satelliteId'> &
+    Required<Pick<SatelliteParameters, 'satelliteId'>>;
 }): Promise<BuildType | undefined> => {
   const status = await canisterMetadata({...rest, canisterId: satelliteId, path: 'juno:build'});
 

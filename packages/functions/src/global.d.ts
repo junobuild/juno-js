cfab4474f15b266bc2794e965ad6d361f6c1da63
David Dal Busco <david.dalbusco@outlook.com>
2025-04-16 09:25:36 +0200

    chore: no dev

--- packages/functions/src/global.d.ts
+++ packages/functions/src/global.d.ts
@@ -6,9 +6,6 @@ import type {Collection, Key, RawPrincipal, RawUserId} from './schemas/satellite
 import type {Controllers} from './sdk/schemas/controllers';
 
 declare global {
-  function __juno_satellite_datastore_raw_data_to_text(data: RawData): string;
-  function __juno_satellite_datastore_raw_data_from_text(data: string): RawData;
-
   function __juno_satellite_get_admin_controllers(): Controllers;
   function __juno_satellite_get_controllers(): Controllers;
   function __juno_satellite_is_admin_controller(
@@ -16,6 +13,11 @@ declare global {
     controllers: Controllers
   ): boolean;
   function __juno_satellite_is_controller(caller: RawUserId, controllers: Controllers): boolean;
+
+  function __juno_satellite_random(): number;
+
+  function __juno_satellite_datastore_raw_data_to_text(data: RawData): string;
+  function __juno_satellite_datastore_raw_data_from_text(data: string): RawData;
 
   function __juno_satellite_datastore_count_collection_docs_store(collection: Collection): bigint;
   function __juno_satellite_datastore_count_docs_store(
@@ -58,8 +60,6 @@ declare global {
     collection: Collection,
     filter: ListParams
   ): bigint;
-
-  function __juno_satellite_random(): number;
 
   function __ic_cdk_id(): RawPrincipal;
   function __ic_cdk_print(msg: string): void;

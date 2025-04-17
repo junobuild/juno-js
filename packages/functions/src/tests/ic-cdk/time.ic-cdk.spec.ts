2f818e5e553189fbf52b30a2243bb9d5a0521cd3
David Dal Busco <david.dalbusco@outlook.com>
2025-04-17 19:54:54 +0200

    feat: ic_cdk::time()

---
+++ packages/functions/src/tests/ic-cdk/time.ic-cdk.spec.ts
@@ -0,0 +1,21 @@
+import {nowInBigIntNanoSeconds} from '@dfinity/utils';
+import {time} from '../../ic-cdk/time.ic-cdk';
+
+vi.stubGlobal('__ic_cdk_time', vi.fn());
+
+describe('ic-cdk > time', () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it('should return a timestamp', () => {
+    const now = nowInBigIntNanoSeconds();
+
+    vi.mocked(global.__ic_cdk_time).mockReturnValue(now);
+
+    const result = time();
+
+    expect(result).toBeInstanceOf(BigInt);
+    expect(result).toEqual(now);
+  });
+});

5ff7f42d2069d331a91f58fc1db1a4f9a296358c
David Dal Busco <david.dalbusco@outlook.com>
2025-05-01 06:39:22 +0200

    feat: return undefined

--- packages/analytics/src/services/user-agent.services.ts
+++ packages/analytics/src/services/user-agent.services.ts
@@ -3,6 +3,11 @@ import {isNullish} from '../utils/dfinity/nullish.utils';
 
 export class UserAgentServices {
   async parseUserAgent(user_agent: string | undefined): Promise<PageViewClient | undefined> {
+    // In test, parsing undefined agent returns WebKit on linux
+    if (isNullish(user_agent)) {
+      return undefined;
+    }
+
     const {UAParser} = await import('ua-parser-js');
 
     const parser = new UAParser(user_agent);

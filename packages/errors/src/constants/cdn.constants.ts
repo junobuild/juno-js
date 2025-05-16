1f56ddd33e67db48dd7610037e4c65b8d5ad6845
David Dal Busco <david.dalbusco@outlook.com>
2025-05-16 17:44:50 +0200

    feat: cdn constants

---
+++ packages/errors/src/constants/cdn.constants.ts
@@ -0,0 +1,21 @@
+export const JUNO_ERROR_PROPOSALS_CANNOT_SUBMIT = 'juno.error.proposals.cannot_submit';
+export const JUNO_ERROR_PROPOSALS_CANNOT_SUBMIT_INVALID_STATUS =
+  'juno.error.proposals.cannot_submit_invalid_status';
+
+export const JUNO_ERROR_PROPOSALS_CANNOT_COMMIT = 'juno.error.proposals.cannot_commit';
+export const JUNO_ERROR_PROPOSALS_CANNOT_COMMIT_INVALID_STATUS =
+  'juno.error.proposals.cannot_commit_invalid_status';
+export const JUNO_ERROR_PROPOSALS_INVALID_HASH = 'juno.error.proposals.invalid_hash';
+
+export const JUNO_ERROR_PROPOSALS_UNKNOWN_TYPE = 'juno.error.proposals.unknown_type';
+
+export const JUNO_ERROR_PROPOSALS_EMPTY_CONTENT_CHUNKS =
+  'juno.error.proposals.empty_content_chunks';
+export const JUNO_ERROR_PROPOSALS_NOT_CONTENT_CHUNKS_AT_INDEX =
+  'juno.error.proposals.no_content_chunks_at_index';
+
+export const JUNO_ERROR_PROPOSALS_EMPTY_ASSETS = 'juno.error.proposals.empty_assets';
+export const JUNO_ERROR_PROPOSALS_CANNOT_DELETE_ASSETS =
+  'juno.error.proposals.cannot_delete_assets';
+export const JUNO_ERROR_PROPOSALS_CANNOT_DELETE_ASSETS_INVALID_STATUS =
+  'juno.error.proposals.cannot_delete_assets_invalid_status';
--- packages/errors/src/index.ts
+++ packages/errors/src/index.ts
@@ -1,4 +1,5 @@
-export * from './constants/satellite.constants';
+export * from './constants/cdn.constants';
 export * from './constants/collections.constants';
+export * from './constants/satellite.constants';
 export * from './constants/shared.constants';
 export * from './utils';

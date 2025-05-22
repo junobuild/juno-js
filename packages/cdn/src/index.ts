365a47aa01fe9d83876caff4d2dd65ab475fc905
David Dal Busco <david.dalbusco@outlook.com>
2025-05-22 11:50:00 +0200

    feat: copy console logic to cdn (#384)

--- packages/cdn/src/index.ts
+++ packages/cdn/src/index.ts
@@ -1,1 +1,5 @@
-export default {};
+export type * from '../declarations/console/console.did';
+export * from './api/proposal.api';
+export * from './api/storage.api';
+export type * from './types/actor.types';
+export type * from './types/console.types';

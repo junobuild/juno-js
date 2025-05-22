57177030f7f17f114eb9b397a677747384d1d994
David Dal Busco <david.dalbusco@outlook.com>
2025-05-22 12:04:07 +0200

    feat: deprecate junobuild/console (#385)
    * feat: deprecate junobuild/console
    
    * feat: deprecate junobuild/console

--- packages/console/src/index.ts
+++
@@ -1,5 +0,0 @@
-export type * from '../declarations/console/console.did';
-export * from './api/proposal.api';
-export * from './api/storage.api';
-export type * from './types/actor.types';
-export type * from './types/console.types';

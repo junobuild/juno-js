762b5056b1d744ff6e39e65258d12d2061a52a9d
David Dal Busco <david.dalbusco@outlook.com>
2025-05-25 20:47:14 +0200

    feat: rename constants

--- packages/errors/src/constants/cdn.constants.ts
+++ packages/errors/src/constants/cdn.constants.ts
@@ -1,24 +1,24 @@
-export const JUNO_ERROR_PROPOSALS_CANNOT_SUBMIT = 'juno.error.proposals.cannot_submit';
-export const JUNO_ERROR_PROPOSALS_CANNOT_SUBMIT_INVALID_STATUS =
-  'juno.error.proposals.cannot_submit_invalid_status';
+export const JUNO_CDN_PROPOSALS_ERROR_CANNOT_SUBMIT = 'juno.cdn.proposals.error.cannot_submit';
+export const JUNO_CDN_PROPOSALS_ERROR_CANNOT_SUBMIT_INVALID_STATUS =
+  'juno.cdn.proposals.error.cannot_submit_invalid_status';
 
-export const JUNO_ERROR_PROPOSALS_CANNOT_COMMIT = 'juno.error.proposals.cannot_commit';
-export const JUNO_ERROR_PROPOSALS_CANNOT_COMMIT_INVALID_STATUS =
-  'juno.error.proposals.cannot_commit_invalid_status';
-export const JUNO_ERROR_PROPOSALS_INVALID_HASH = 'juno.error.proposals.invalid_hash';
+export const JUNO_CDN_PROPOSALS_ERROR_CANNOT_COMMIT = 'juno.cdn.proposals.error.cannot_commit';
+export const JUNO_CDN_PROPOSALS_ERROR_CANNOT_COMMIT_INVALID_STATUS =
+  'juno.cdn.proposals.error.cannot_commit_invalid_status';
+export const JUNO_CDN_PROPOSALS_ERROR_INVALID_HASH = 'juno.cdn.proposals.error.invalid_hash';
 
-export const JUNO_ERROR_PROPOSALS_UNKNOWN_TYPE = 'juno.error.proposals.unknown_type';
+export const JUNO_CDN_PROPOSALS_ERROR_UNKNOWN_TYPE = 'juno.cdn.proposals.error.unknown_type';
 
-export const JUNO_ERROR_PROPOSALS_EMPTY_CONTENT_CHUNKS =
-  'juno.error.proposals.empty_content_chunks';
-export const JUNO_ERROR_PROPOSALS_NOT_CONTENT_CHUNKS_AT_INDEX =
-  'juno.error.proposals.no_content_chunks_at_index';
+export const JUNO_CDN_PROPOSALS_ERROR_EMPTY_CONTENT_CHUNKS =
+  'juno.cdn.proposals.error.empty_content_chunks';
+export const JUNO_CDN_PROPOSALS_ERROR_NOT_CONTENT_CHUNKS_AT_INDEX =
+  'juno.cdn.proposals.error.no_content_chunks_at_index';
 
-export const JUNO_ERROR_PROPOSALS_EMPTY_ASSETS = 'juno.error.proposals.empty_assets';
-export const JUNO_ERROR_PROPOSALS_CANNOT_DELETE_ASSETS =
-  'juno.error.proposals.cannot_delete_assets';
-export const JUNO_ERROR_PROPOSALS_CANNOT_DELETE_ASSETS_INVALID_STATUS =
-  'juno.error.proposals.cannot_delete_assets_invalid_status';
+export const JUNO_CDN_PROPOSALS_ERROR_EMPTY_ASSETS = 'juno.cdn.proposals.error.empty_assets';
+export const JUNO_CDN_PROPOSALS_ERROR_CANNOT_DELETE_ASSETS =
+  'juno.cdn.proposals.error.cannot_delete_assets';
+export const JUNO_CDN_PROPOSALS_ERROR_CANNOT_DELETE_ASSETS_INVALID_STATUS =
+  'juno.cdn.proposals.error.cannot_delete_assets_invalid_status';
 
 export const JUNO_CDN_STORAGE_ERROR_CANNOT_INSERT_ASSET_UNKNOWN_REFERENCE_ID =
   'juno.cdn.storage.error.cannot_insert_asset_unknown_reference_id';

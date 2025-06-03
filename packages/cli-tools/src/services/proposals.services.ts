9d97ce324b2586784a6911c626e21d035059f85c
David Dal Busco <david.dalbusco@outlook.com>
2025-06-03 10:03:11 +0200

    fix: use snake case param

--- packages/cli-tools/src/services/proposals.services.ts
+++ packages/cli-tools/src/services/proposals.services.ts
@@ -38,7 +38,7 @@ export const proposeChanges = async ({
 
   const [__, {sha256: proposalSha256, status}] = await submitProposal({
     cdn,
-    proposalId
+    proposal_id: proposalId
   });
 
   const sha256 = fromNullable(proposalSha256);

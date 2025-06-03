ec1b7579736f3905cd3f2055e2d13221a618051c
David Dal Busco <david.dalbusco@outlook.com>
2025-06-03 13:16:44 +0200

    chore: fmt

--- packages/cli-tools/src/index.ts
+++ packages/cli-tools/src/index.ts
@@ -10,7 +10,7 @@ export * from './utils/args.utils';
 export * from './utils/cmd.utils';
 export * from './utils/compress.utils';
 export * from './utils/download.utils';
+export * from './utils/format.utils';
 export * from './utils/fs.utils';
 export * from './utils/pkg.utils';
-export * from './utils/format.utils';
 export * from './utils/prompt.utils';

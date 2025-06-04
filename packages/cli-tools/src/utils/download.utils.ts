2aacb5a7839605ced01708833458555d60f90783
David Dal Busco <david.dalbusco@outlook.com>
2025-06-04 12:25:16 +0200

    feat: extend downloadFromURL to support http for local dev

--- packages/cli-tools/src/utils/download.utils.ts
+++ packages/cli-tools/src/utils/download.utils.ts
@@ -1,8 +1,12 @@
 import {nonNullish} from '@dfinity/utils';
-import {get, type RequestOptions} from 'https';
+import {get as httpGet} from 'http';
+import {get as httpsGet, type RequestOptions} from 'https';
 
 export const downloadFromURL = async (url: string | RequestOptions): Promise<Buffer> =>
   await new Promise((resolve, reject) => {
+    const protocol = typeof url === 'string' ? URL.parse(url)?.protocol : url.protocol;
+    const get = protocol === 'http' ? httpGet : httpsGet;
+
     get(url, async (res) => {
       if (
         nonNullish(res.statusCode) &&

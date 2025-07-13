584773828e777acc8ac8fc57a1cb85ee9329eaac
David Dal Busco <david.dalbusco@outlook.com>
2025-07-13 11:59:07 +0200

    test: more tests

---
+++ packages/core/src/tests/utils/auth.utils.spec.ts
@@ -0,0 +1,24 @@
+import {AuthClient} from '@dfinity/auth-client';
+import type {Mock} from 'vitest';
+import {createAuthClient} from '../../utils/auth.utils';
+
+vi.mock('@dfinity/auth-client', () => ({
+  AuthClient: {
+    create: vi.fn()
+  }
+}));
+
+describe('auth.utils', () => {
+  it('passes the correct idle options to AuthClient.create', async () => {
+    (AuthClient.create as Mock).mockResolvedValue({} as AuthClient);
+
+    await createAuthClient();
+
+    expect(AuthClient.create).toHaveBeenCalledWith({
+      idleOptions: {
+        disableIdle: true,
+        disableDefaultIdleCallback: true
+      }
+    });
+  });
+});

c511cbbc055f0f702a5fc8617f38a7efb03aabc7
David Dal Busco <david.dalbusco@outlook.com>
2025-07-14 06:51:02 +0200

    chore: revert

--- packages/core/src/api/_actor.factory.ts
+++ packages/core/src/api/_actor.factory.ts
@@ -2,8 +2,6 @@ import type {_SERVICE as SatelliteActor} from '../../declarations/satellite/sate
 // eslint-disable-next-line import/no-relative-parent-imports
 import {idlFactory as idlFactorySatellite} from '../../declarations/satellite/satellite.factory.did.js';
 // eslint-disable-next-line import/no-relative-parent-imports
-import {idlFactory as idlCertifiedFactorySatellite} from '../../declarations/satellite/satellite.factory.certified.did.js';
-// eslint-disable-next-line import/no-relative-parent-imports
 import {idlFactory as idlFactorySputnik} from '../../declarations/sputnik/sputnik.factory.did.js';
 
-export {idlCertifiedFactorySatellite, idlFactorySatellite, idlFactorySputnik, type SatelliteActor};
+export {idlFactorySatellite, idlFactorySputnik, type SatelliteActor};

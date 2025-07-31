3cfd55afcdff3778f713bdf136a2b75fb26c9610
David Dal Busco <david.dalbusco@outlook.com>
2025-07-31 07:33:57 +0200

    feat(cli-tools): compress HTML

--- packages/cli-tools/src/constants/deploy.constants.ts
+++ packages/cli-tools/src/constants/deploy.constants.ts
@@ -1,7 +1,7 @@
 export const DEPLOY_DEFAULT_SOURCE = 'build';
 export const DEPLOY_DEFAULT_IGNORE = [];
 export const DEPLOY_DEFAULT_ENCODING = [];
-export const DEPLOY_DEFAULT_GZIP = '**/*.+(css|js|mjs)';
+export const DEPLOY_DEFAULT_GZIP = '**/*.+(css|js|mjs|html)';
 
 export const IGNORE_OS_FILES = ['.ds_store', 'thumbs.db'];
 export const UPLOAD_BATCH_SIZE = 20;

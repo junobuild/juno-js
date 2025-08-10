143e642f9866488ce441ab762b953cd58a8048e9
David Dal Busco <david.dalbusco@outlook.com>
2025-08-10 10:51:00 +0200

    feat(ic-agent): copy actor and agent from admin

--- packages/ic-client/src/index.ts
+++ packages/ic-client/src/index.ts
@@ -1,1 +1,2 @@
-export default {};
+export * from './api/_actor.api';
+export * from './api/_actor.factory';
---
+++ packages/ic-client/src/utils/agent.utils.ts
@@ -0,0 +1,16 @@
+import {HttpAgent} from '@dfinity/agent';
+import type {ActorParameters} from '../types/actor';
+
+// We have this in a utils because we want to mock it for test purposes.
+// Not really happy with that, I welcome better ideas!
+export const createAgent = async ({
+  identity,
+  host,
+  localActor
+}: Pick<ActorParameters, 'identity'> & {host: string; localActor: boolean}) =>
+  await HttpAgent.create({
+    identity,
+    host,
+    retryTimes: 10,
+    shouldFetchRootKey: localActor
+  });

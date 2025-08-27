ec061c7aff09fc9813eb01b271a5b908fc7a321a
David Dal Busco <david.dalbusco@outlook.com>
2025-08-27 14:47:28 +0200

    feat(ic-client): progress contains an enum

--- packages/ic-client/src/webauthn.ts
+++ packages/ic-client/src/webauthn.ts
@@ -4,5 +4,5 @@ export * from './webauthn/errors';
 export * from './webauthn/identity';
 export type * from './webauthn/types/identity';
 export type * from './webauthn/types/passkey';
-export type * from './webauthn/types/progress';
+export * from './webauthn/types/progress';
 export * from './webauthn/utils';

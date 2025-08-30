75a13bd6f172c8fcfb98dbb820a70dc25b875f05
David Dal Busco <david.dalbusco@outlook.com>
2025-08-30 09:54:12 +0200

    test: signIn

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,7 +1,13 @@
 import type {HttpAgent} from '@dfinity/agent';
 import 'fake-indexeddb/auto';
+import {Blob as BlobPolyfill} from 'node:buffer';
 import {vi} from 'vitest';
 import {mock} from 'vitest-mock-extended';
+
+// JSDOM limitation
+// https://github.com/jsdom/jsdom/issues/2555
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+global.Blob = BlobPolyfill as any;
 
 vi.mock(import('./packages/core/src/core/stores/_agent.factory'), async (importOriginal) => {
   const actual = await importOriginal();

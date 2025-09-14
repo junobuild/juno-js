15e90d1498c5f55da8482d6699c0ea94002fcad5
David Dal Busco <david.dalbusco@outlook.com>
2025-09-14 09:39:39 +0200

    feat(ic-client): encodedKey can be readonly private instead of protected (#576)

--- packages/ic-client/src/webauthn/agent-js/cose-key.ts
+++ packages/ic-client/src/webauthn/agent-js/cose-key.ts
@@ -5,7 +5,7 @@ import {_coseToDerEncodedBlob} from './cose-utils';
 /**
  * ⚠️ !!!WARNING!!! ⚠️
  * This module is a copy/paste of the webauthn classes not exposed by Agent-js
- * extended with mandatory toRaw().
+ * extended with mandatory toRaw() and encodedKey made private.
  * It is therefore not covered by that many tests (‼️) in this library.
  *
  * @see https://github.com/dfinity/agent-js/blob/main/packages/identity/src/identity/webauthn.ts
@@ -18,17 +18,17 @@ import {_coseToDerEncodedBlob} from './cose-utils';
 export type CoseEncodedKey = Uint8Array;
 
 export class CosePublicKey implements PublicKeyWithToRaw {
-  protected _encodedKey: DerEncodedPublicKey;
+  readonly #encodedKey: DerEncodedPublicKey;
 
   public constructor(protected _cose: CoseEncodedKey) {
-    this._encodedKey = _coseToDerEncodedBlob(_cose);
+    this.#encodedKey = _coseToDerEncodedBlob(_cose);
   }
 
   public toDer(): DerEncodedPublicKey {
-    return this._encodedKey;
+    return this.#encodedKey;
   }
 
   public toRaw(): Uint8Array {
-    return new Uint8Array(this._encodedKey); // Strip __derEncodedPublicKey__
+    return new Uint8Array(this.#encodedKey); // Strip __derEncodedPublicKey__
   }
 }

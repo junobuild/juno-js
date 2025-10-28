3bb08c9d65a3011cbd79bc75f08cdc08c86c6d19
David Dal Busco <david.dalbusco@outlook.com>
2025-10-28 12:39:17 +0100

    feat(errors): openid

--- packages/errors/src/constants/satellite.constants.ts
+++ packages/errors/src/constants/satellite.constants.ts
@@ -30,6 +30,8 @@ export const JUNO_AUTH_ERROR_INVALID_ORIGIN = 'juno.auth.error.invalid_origin';
 export const JUNO_AUTH_ERROR_NOT_WRITE_CONTROLLER = 'juno.auth.error.not_write_controller';
 export const JUNO_AUTH_ERROR_NOT_CONTROLLER = 'juno.auth.error.not_controller';
 export const JUNO_AUTH_ERROR_CALLER_NOT_ALLOWED = 'juno.auth.error.caller.not_allowed';
+export const JUNO_AUTH_ERROR_NOT_CONFIGURED = 'juno.auth.error.not_configured';
+export const JUNO_AUTH_ERROR_OPENID_DISABLED = 'juno.auth.error.openid_disabled';
 
 export const JUNO_DATASTORE_ERROR_USER_CANNOT_UPDATE = 'juno.datastore.error.user.cannot_update';
 export const JUNO_DATASTORE_ERROR_USER_INVALID_DATA = 'juno.datastore.error.user.invalid_data';

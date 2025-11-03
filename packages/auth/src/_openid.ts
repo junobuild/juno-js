374170168ec7d1547fed11e26ff77dc1194117f9
David Dal Busco <david.dalbusco@outlook.com>
2025-11-03 06:10:39 +0100

    feat(auth): bubble url in cause

--- packages/auth/src/_openid.ts
+++ packages/auth/src/_openid.ts
@@ -27,7 +27,7 @@ export const requestJwtWithRedirect = ({
       // Use the URL constructor, for backwards compatibility with older Android/WebView.
       return new URL(authUrl);
     } catch (error: unknown) {
-      throw new InvalidUrlError('Cannot parse authURL', {cause: error});
+      throw new InvalidUrlError('Cannot parse authURL', {cause: authUrl});
     }
   };
 

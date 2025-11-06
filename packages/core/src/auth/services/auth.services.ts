1948c0e736e09db756a5912eb32a1cd0748a2dca
David Dal Busco <david.dalbusco@outlook.com>
2025-11-06 10:03:10 +0100

    test: redirects

--- packages/core/src/auth/services/auth.services.ts
+++ packages/core/src/auth/services/auth.services.ts
@@ -4,7 +4,7 @@ import type {Identity} from '@icp-sdk/core/agent';
 import {ActorStore} from '../../core/stores/actor.store';
 import {AgentStore} from '../../core/stores/agent.store';
 import {executeWithWindowGuard} from '../helpers/window.helpers';
-import {GoogleProvider} from '../providers/google.provider';
+import {GoogleProvider} from '../providers/google.providers';
 import {InternetIdentityProvider} from '../providers/internet-identity.providers';
 import {WebAuthnProvider} from '../providers/webauthn.providers';
 import {AuthStore} from '../stores/auth.store';

1dc2502f84bba5c4bc7f08f0980a463d623a2556
David Dal Busco <david.dalbusco@outlook.com>
2026-01-17 08:47:08 +0100

    feat: github auth url

--- packages/auth/src/_constants.ts
+++ packages/auth/src/_constants.ts
@@ -11,7 +11,7 @@ export const GOOGLE_PROVIDER: Omit<OpenIdProvider, 'clientId' | 'redirectUrl'> =
 };
 
 export const GITHUB_PROVIDER: Omit<OpenIdGitHubProvider, 'clientId' | 'redirectUrl'> = {
-  authUrl: 'http://accounts.google.com/o/o/oauth2/v2/auth',
+  authUrl: 'https://github.com/login/oauth/authorize',
   authScopes: ['read:user', 'repo'],
   initUrl: 'http://localhost:3000/v1/auth/init/github',
   finalizeUrl: 'http://localhost:3000/v1/auth/finalize/github'

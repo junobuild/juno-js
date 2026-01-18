22b7eea11d034b8bba490f3f0e1daf1fe5e1f770
David Dal Busco <david.dalbusco@outlook.com>
2026-01-18 20:27:15 +0100

    feat(auth): extract google authenticate with redirect (#714)

--- packages/auth/src/authenticate.ts
+++ packages/auth/src/authenticate.ts
@@ -1,17 +1,11 @@
-import {isEmptyString} from '@dfinity/utils';
 import {loadContext} from './_context';
 import {authenticateSession} from './_session';
-import {
-  AuthenticationInvalidStateError,
-  AuthenticationUndefinedJwtError,
-  AuthenticationUrlHashError
-} from './errors';
+import {authenticateGoogleWithRedirect} from './providers/google/authenticate';
 import type {
   AuthenticatedSession,
   AuthenticationParams,
   AuthParameters
 } from './types/authenticate';
-import type {OpenIdAuthContext} from './types/context';
 
 export const authenticate = async <T extends AuthParameters>(
   params: AuthenticationParams<T>
@@ -32,41 +26,4 @@ export const authenticate = async <T extends AuthParameters>(
   }
 
   return await authenticateGoogleWithRedirect<T>({...params, context});
-};
-
-const authenticateGoogleWithRedirect = async <T extends AuthParameters>({
-  auth,
-  context
-}: {
-  auth: AuthParameters;
-  context: OpenIdAuthContext;
-}): Promise<AuthenticatedSession<T>> => {
-  const {
-    location: {hash}
-  } = window;
-
-  if (isEmptyString(hash) || !hash.startsWith('#')) {
-    throw new AuthenticationUrlHashError('No hash found in the current location URL');
-  }
-
-  const params = new URLSearchParams(hash.slice(1));
-  const state = params.get('state');
-  const idToken = params.get('id_token');
-
-  const {state: savedState} = context;
-
-  if (isEmptyString(savedState) || state !== savedState) {
-    throw new AuthenticationInvalidStateError('The provided state is invalid', {cause: state});
-  }
-
-  // id_token === jwt
-  if (isEmptyString(idToken)) {
-    throw new AuthenticationUndefinedJwtError();
-  }
-
-  return await authenticateSession({
-    jwt: idToken,
-    auth,
-    context
-  });
 };

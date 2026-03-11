aba648764e15aa2ab7339a1f91908448eed7d03b
David Dal Busco <david.dalbusco@outlook.com>
2026-03-11 19:18:15 +0100

    feat(functions): introduce canisterSelf and mark id as deprecated

--- packages/functions/src/ic-cdk/id.ic-cdk.ts
+++ packages/functions/src/ic-cdk/id.ic-cdk.ts
@@ -4,12 +4,12 @@ import {Principal} from '@icp-sdk/core/principal';
  * Retrieves the Satellite's Principal ID.
  *
  * This function is a JavaScript binding for the Rust function
- * [`ic_cdk::id()`](https://docs.rs/ic-cdk/latest/ic_cdk/fn.id.html), which returns
+ * [`ic_cdk::api::canister_self()`](https://docs.rs/ic-cdk/latest/ic_cdk/api/fn.canister_self.html), which returns
  * the Principal of the executing canister.
  *
  * @returns {Principal} The Principal ID of the Satellite.
  */
-export const id = (): Principal => {
+export const canisterSelf = (): Principal => {
   const principal = Principal.fromUint8Array(__ic_cdk_id());
 
   // We assume the Principal is always built using the global function and is therefore always valid.
@@ -17,3 +17,15 @@ export const id = (): Principal => {
 
   return principal;
 };
+
+/**
+ * @deprecated Use {@link canisterSelf} instead.
+ */
+export const id = (): Principal => canisterSelf();
+
+/**
+ * Retrieves the Satellite's Principal ID.
+ *
+ * @returns {Principal} The Principal ID of the Satellite.
+ */
+export const satelliteSelf = (): Principal => canisterSelf();

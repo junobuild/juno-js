a31f7ec0a526df17de70bf3089a25e53e58771b0
David Dal Busco <david.dalbusco@outlook.com>
2026-03-15 12:20:57 +0100

    chore: no dev

--- packages/cli-tools/src/commands/deploy.ts
+++ packages/cli-tools/src/commands/deploy.ts
@@ -20,7 +20,6 @@ import type {
 } from '../types/deploy';
 import type {ProposeChangesParams} from '../types/proposal';
 import type {UploadFilesParams} from '../types/upload';
-
 import {fullPath} from '../utils/assets.utils';
 
 /**

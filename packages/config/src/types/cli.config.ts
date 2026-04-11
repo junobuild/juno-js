d96410e30a4d382bb9868f43daf2ec6e42816395
David Dal Busco <david.dalbusco@outlook.com>
2026-04-11 14:01:06 +0200

    feat: replace type alias

--- packages/config/src/types/cli.config.ts
+++ packages/config/src/types/cli.config.ts
@@ -1,6 +1,5 @@
 import * as z from 'zod';
 import {type HostingConfig, HostingConfigSchema} from './hosting.config';
-import type {Deprecated} from './utility.types';
 
 const {source, ...rest} = HostingConfigSchema.shape;
 
@@ -16,4 +15,17 @@ export const CliConfigSchema = z.strictObject({
 /**
  * @deprecated use HostingConfig
  */
-export type CliConfig = Deprecated<HostingConfig>;
+export interface CliConfig {
+  /** @deprecated use HostingConfig.source */
+  source?: HostingConfig['source'];
+  /** @deprecated use HostingConfig.ignore */
+  ignore?: HostingConfig['ignore'];
+  /** @deprecated use HostingConfig.precompress */
+  precompress?: HostingConfig['precompress'];
+  /** @deprecated use HostingConfig.encoding */
+  encoding?: HostingConfig['encoding'];
+  /** @deprecated use HostingConfig.predeploy */
+  predeploy?: HostingConfig['predeploy'];
+  /** @deprecated use HostingConfig.postdeploy */
+  postdeploy?: HostingConfig['postdeploy'];
+}
